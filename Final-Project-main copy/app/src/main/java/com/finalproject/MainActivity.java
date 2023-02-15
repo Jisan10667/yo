74232d0b354bde7d920d0d01102cdfd7796125cb
Jisan Ahmed <jisanahmed10667@gmail.com>
2023-02-15 21:29:44 +0600

    checkk

---
+++ Final-Project-main copy/app/src/main/java/com/finalproject/MainActivity.java
@@ -0,0 +1,38 @@
+package com.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.getstart);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
